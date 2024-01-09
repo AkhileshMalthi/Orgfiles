1debe54613462e1059afb5f4d6774178aef454c0
Akhilesh <119729949+AkhileshMalthi@users.noreply.github.com>
2024-01-09 21:58:02 +0530

    Separate util functions from app.py

--- src/app.py
+++ src/app.py
@@ -3,20 +3,9 @@ import typer
 import os
 import shutil
 import json
+from utils import *
 
 app = typer.Typer()
-MOVEMENTS_FILE = "file_movements.json"
-
-def save_movements(movements):
-    with open(MOVEMENTS_FILE, "w") as file:
-        json.dump(movements, file, indent=4)
-
-def load_movements():
-    movements = {}
-    if os.path.exists(MOVEMENTS_FILE):
-        with open(MOVEMENTS_FILE, "r") as file:
-            movements = json.load(file)
-    return movements
 
 @app.command()
 def organize(directory: str = typer.Argument(..., help="The directory path where the operation will take place."),
